20e2f9ce028ef3a3b733b7b206e7410d714aed24
VisTech Kamil <144006221+Vistechnologie@users.noreply.github.com>
2024-12-12 15:48:48 +0100

    Add recipe management functionality with models, serializers, views, and tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,10 +1,12 @@
 """
 Tests for the models of the core app.
 """
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
 
 class ModelTests(TestCase):
     """Test the core app models."""
@@ -57,3 +59,20 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a new recipe."""
+
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=Decimal('5.48'),
+            description='Cook the steak and the mushroom sauce',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
